289d78ee55e0c9b49c3785015809199a439b40e6
MohamedHamdy <midohamdi487@gmail.com>
2025-06-13 16:46:25 +0300

    Make the relation between cart and CarItem bidirectional

--- Shopito/src/main/java/com/example/Shopito/Entities/cart/Cart.java
+++ Shopito/src/main/java/com/example/Shopito/Entities/cart/Cart.java
@@ -4,6 +4,9 @@ import com.example.Shopito.Entities.users;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Entity
 public class Cart {
@@ -16,4 +19,11 @@ public class Cart {
     @JoinColumn(name = "user_id",nullable = false)
 
     private users user;
+
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+
+
+
 }
